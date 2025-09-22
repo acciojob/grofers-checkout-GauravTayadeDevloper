23b3f28880bfa3a62066f81891e41b3d0394d4d5
GAURAV  TAYADE <110455312+GauravTayadeDevloper@users.noreply.github.com>
2025-09-22 10:42:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,23 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+      let totalPrice = document.querySelectorAll(".price"); 
+      let sum = 0;
+      totalPrice.forEach(price => {
+        sum += parseInt(price.textContent, 10);
+      });
+
+      let table = document.getElementsByTagName("table")[0];
+      let row = document.createElement("tr");
+
+      // requirement: single cell showing total
+      let cell = document.createElement("td");
+      cell.id="ans";
+      cell.setAttribute("colspan", "2"); // span across both columns
+      cell.textContent = "Total Price = " + sum;
+
+      row.appendChild(cell);
+      table.appendChild(row);
 };
 
 getSumBtn.addEventListener("click", getSum);
